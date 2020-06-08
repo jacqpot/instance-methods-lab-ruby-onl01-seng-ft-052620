3393d6fee115d194a3656c48d4cfa6b17c50dd5e
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-08 16:55:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,13 @@
-class Dog 
+# class Dog 
   
-  def bark
-    puts "Woof!"
-  end
-  def sit 
-    puts "The Dog is sitting"
-  end
+#   def bark
+#     puts "Woof!"
+#   end
+#   def sit 
+#     puts "The Dog is sitting"
+#   end
   
   
-end 
+# end 
+
+
